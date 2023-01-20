06e404c4c5e5df7e19a86e57496072246b216dd7
Stephen Justin <stevejustin1963@gmail.com>
2023-01-21 09:53:25 +1100

    Update lift1.c

--- lift1.c
+++ lift1.c
@@ -1,30 +1,4 @@
-https://gregoryalfordblog.wordpress.com/2017/05/30/pseudocoding-an-elevator/
 
-The building is 25 stories high and will respond to only the following commands:
-
-“open elevator door”
-“close elevator door”
-“go up full speed”
-“go down full speed”
-“slow down”
-“stop”
-The elevator accepts user input for:
-
-floor buttons inside the elevator
-door open and close buttons inside the elevator
-up and down call buttons on each floor
-Has sensors for:
-
-if a human is in the door closing path
-if it is currently at a floor (instead of in-between floors)
-Has a few quirky requirements:
-
-it must “slow down” at least 1 floor before it stops.
-there is a small chance that it actually stops between floors by accident (it’s an old elevator)
-
-write a c program to control the lift
-==============================================
- 
 
 #include <stdio.h>
 #include <stdlib.h>
